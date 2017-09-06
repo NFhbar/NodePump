99fbe148663010034f7584e8c7d0ac56b059f17c
NFhbar <frega.nicolas@gmail.com>
2017-09-05 22:48:32 -0700

    added functions

---
+++ index.js
@@ -0,0 +1,21 @@
+//==============================================
+//INSTALLATION
+//==============================================
+// install node
+// install npm
+// move to directory
+// : npm init
+// : npm install
+// : npm install --save request
+// : npm install --save request-promise
+// : node index.js
+//==============================================
+
+//==============================================
+//RUN
+//==============================================
+//require the menuRunner() function
+var MenuRunner = require("./functions/menuRunner");
+
+//run the menuRunner() function
+MenuRunner.menuRunner();
